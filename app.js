e066dfdd8b396b87cb0a63ba3677f71c9a0c8b06
Ashley Adams <61568352+ashleyxshanice@users.noreply.github.com>
2022-08-16 02:29:11 -0400

    Update app.js to include MongoDB sample code.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-test:Test123@cluster0.umcstm1.mongodb.net/todolistDB");
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.umcstm1.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
